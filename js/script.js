6bbd8a8af5749805db2af9e8253d21f8a350911d
Anderson <anderson.scastilho@hotmail.com>
2022-07-20 13:25:21 -0300

    Renderizando o nome do pokemon na tela

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-const fetchpokemon = async (pokemon)=>{
+const pokemonName = document.querySelector('.pokemon_name');
 
+const fetchpokemon = async (pokemon)=>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
-    console.log(data)
     console.log(APIResponse);
     return data;
 }
 const renderPokemon = async (pokemon)=>{
     const data = await fetchpokemon(pokemon)
+    pokemonName.innerHtml = data.name
+    
 }
-fetchpokemon(25)
+fetchpokemon('25')
