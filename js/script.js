6bf1ea14caa0c08a94affde5a6507da896813fa8
Anderson <anderson.scastilho@hotmail.com>
2022-07-20 15:30:15 -0300

    Pokedex criada com sucesso com as instruções do youtuber - Manual do dev

--- js/script.js
+++ js/script.js
@@ -1,14 +1,56 @@
-const pokemonName = document.getElementsByClassName('pokemom_name')
-const pokemonNumber = document.getElementsByClassName('pokemon_number')
-const pokemonImage = document.getElementsByClassName('pokemon_image')
+const pokemonName = document.querySelector('.pokemom_name')
+const pokemonNumber = document.querySelector('.pokemon_number')
+const pokemonImage = document.querySelector('.pokemon_image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input_search')
+
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1
 
 const fetchPokemon = async (pokemon)=>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    const data = await APIResponse.json()
-    return data
+    if(APIResponse.status== 200){
+
+        const data = await APIResponse.json();
+        return data;
+    }
 }
-const renderPokemon = async (pokemon)=>{
+const renderPokemon = async(pokemon) =>{
+
+    pokemonName.innerHTML = 'loading...'
+    pokemonNumber.innerHTML =''
+
     const data = await fetchPokemon(pokemon)
-    
+    if(data){
+        pokemonImage.style.display ='block'
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        input.value = ""
+        searchPokemon = data.id
+    }else{
+        pokemonImage.style.display = 'none'
+        pokemonName.innerHTML = 'Not found :c'
+        pokemonNumber.innerHTML = ''
+    }
 }
-renderPokemon(25)
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    renderPokemon(input.value.toLowerCase())
+})
+
+buttonPrev.addEventListener('click', ()=>{
+    if(searchPokemon > 1 ){
+        searchPokemon -=1
+        renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', ()=>{
+    searchPokemon += 1
+    renderPokemon(searchPokemon)
+})
+renderPokemon(searchPokemon)
