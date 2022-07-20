34c4c9baa9ce46dd55fe3c0ae056dde2070e74de
Anderson <anderson.scastilho@hotmail.com>
2022-07-20 14:32:05 -0300

    *

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-const pokemonName = document.querySelector('.pokemon_name');
-const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonName = document.getElementsByClassName('pokemom_name')
+const pokemonNumber = document.getElementsByClassName('pokemon_number')
+const pokemonImage = document.getElementsByClassName('pokemon_image')
 
 const fetchPokemon = async (pokemon)=>{
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const data = await APIResponse.json()
+    return data
 }
 const renderPokemon = async (pokemon)=>{
-    const data = await fetchPokemon(pokemon);
-
-    pokemonNumber.innerHTML = data.id
-    pokemonName.innerHTML = data.name;
+    const data = await fetchPokemon(pokemon)
+    
 }
+renderPokemon(25)
