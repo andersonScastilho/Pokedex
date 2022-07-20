a6d2c77d32467dcc846da888f9d1c211a59229e9
Anderson <anderson.scastilho@hotmail.com>
2022-07-20 13:33:23 -0300

    Testando renderPokemon

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
 
 const fetchPokemon = async (pokemon)=>{
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -6,10 +7,8 @@ const fetchPokemon = async (pokemon)=>{
     return data;
 }
 const renderPokemon = async (pokemon)=>{
-
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name
-    
+    pokemonNumber.innerHTML = data.id
+    pokemonName.innerHTML = data.name;
 }
-renderPokemon(25)
